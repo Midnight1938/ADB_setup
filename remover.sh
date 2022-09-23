a56baa531e98dbec73635945116901350ad27d12
skshm <saksham.sharma11@dlps.co.in>
2022-09-23 12:20:08 +0530

    Bash and readme

---
+++ remover.sh
@@ -0,0 +1,227 @@
+#Facebook
+adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.facebook.katana
+adb shell pm uninstall --user 0 com.facebook.services
+adb shell pm uninstall --user 0 com.facebook.system
+
+#Amazon
+adb shell pm uninstall --user 0 com.amazon.appmanager
+adb shell pm uninstall --user 0 com.amazon.avod.thirdpartyclient
+adb shell pm uninstall --user 0 com.amazon.mShop.android.shopping
+
+# LG
+adb shell pm uninstall --user 0 com.gameloft.android.ANMP.GloftA9HM # racing 9
+adb shell pm uninstall --user 0 com.gameloft.android.ANMP.Gloft5DHM # dungeons
+adb shell pm uninstall --user 0 com.gameloft.android.ANMP.GloftM5HM # wannabe COD
+adb shell pm uninstall --user 0 com.gameloft.android.ANMP.GloftFWHM # wanna sniper elite
+adb shell pm uninstall --user 0 com.instagram.android
+adb shell pm uninstall --user 0 com.lge.lifetracker #LG Health
+adb shell pm uninstall --user 0 com.lge.booking
+adb shell pm uninstall --user 0 com.lge.snappage
+adb shell pm uninstall --user 0 com.naver.whale
+adb shell pm uninstall --user 0 com.lge.music
+adb shell pm uninstall --user 0 com.lgeil.serviceindia
+adb shell pm uninstall -k --user 0 com.lge.myplace.engine
+adb shell pm uninstall -k --user 0 com.lge.myplace
+adb shell pm uninstall --user 0 com.lge.lgworld
+adb shell pm uninstall --user 0 com.rsupport.rs.activity.lge.allinone
+adb shell pm uninstall --user 0 com.lge.lms2
+adb shell pm uninstall --user 0 com.lge.lgaccount
+adb shell pm uninstall --user 0 com.lge.lgmapui
+adb shell pm uninstall --user 0 com.lgeha.nuts
+adb shell pm uninstall --user 0 com.lge.carfinding
+adb shell pm uninstall --user 0 com.lge.qhelp
+adb shell pm uninstall --user 0 com.lge.qhelp.application
+
+# Mi
+adb shell pm uninstall --user 0 cn.wps.moffice_eng
+adb shell pm uninstall --user 0 cn.wps.xiaomi.abroad.lite
+adb shell pm uninstall --user 0 com.alibaba.aliexpresshd
+adb shell pm uninstall --user 0 ru.aliexpress.buyer
+adb shell pm uninstall --user 0 com.allgoritm.youla
+adb shell pm uninstall --user 0 com.android.bookmarkprovider
+adb shell pm uninstall --user 0 com.android.browser
+adb shell pm uninstall --user 0 com.android.providers.downloads.ui
+adb shell pm uninstall --user 0 com.android.soundrecorder
+adb shell pm uninstall --user 0 com.android.stk
+adb shell pm uninstall --user 0 com.android.wallpaper.livepicker
+adb shell pm uninstall --user 0 com.aura.oobe.bouygues
+adb shell pm uninstall --user 0 com.booking
+adb shell pm uninstall --user 0 com.duokan.phone.remotecontroller
+adb shell pm uninstall --user 0 com.ebay.carrier
+adb shell pm uninstall --user 0 com.ebay.mobile
+adb shell pm uninstall --user 0 com.google.android.youtube
+adb shell pm uninstall --user 0 com.google.android.apps.youtube.music
+adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.facemoji.lite.xiaomi
+adb shell pm uninstall --user 0 com.igg.android.lordsmobile
+adb shell pm uninstall --user 0 com.kms.free
+adb shell pm disable-user --user 0 com.mi.globalbrowser
+adb shell pm disable-user --user 0 com.miui.android.fashiongallery
+adb shell pm uninstall --user 0 com.mi.android.globalminusscreen
+adb shell pm uninstall --user 0 com.micredit.in
+adb shell pm uninstall --user 0 com.mi.global.bbs
+adb shell pm uninstall --user 0 com.mi.global.shop
+adb shell pm uninstall --user 0 com.miui.newmidrive
+adb shell pm uninstall --user 0 com.miui.huanji
+adb shell pm uninstall --user 0 com.miui.hybrid
+adb shell pm uninstall --user 0 com.miui.notes
+adb shell pm uninstall --user 0 com.miui.yellowpage
+adb shell pm uninstall --user 0 com.miui.analytics
+adb shell pm uninstall --user 0 com.miui.compass
+adb shell pm uninstall --user 0 com.miui.fm
+adb shell pm uninstall --user 0 com.miui.fmservice
+adb shell pm uninstall --user 0 com.miui.miservice
+adb shell pm uninstall --user 0 com.miui.mishare.connectivity
+adb shell pm uninstall --user 0 com.xiaomi.midrop
+adb shell pm uninstall --user 0 com.miui.msa.global
+adb shell pm uninstall --user 0 com.miui.player
+adb shell pm uninstall --user 0 com.miui.videoplayer
+adb shell pm uninstall --user 0 com.netflix.partner.activation
+adb shell pm uninstall --user 0 com.netflix.mediaclient
+adb shell pm uninstall --user 0 com.opera.browser
+adb shell pm uninstall --user 0 com.opera.preinstall
+adb shell pm uninstall --user 0 com.quicinc.voice.activation
+adb shell pm uninstall --user 0 com.spotify.music
+adb shell pm uninstall --user 0 com.tencent.igxiaomi
+adb shell pm uninstall --user 0 com.xiaomi.joyose
+adb shell pm uninstall --user 0 com.xiaomi.glgm
+adb shell pm uninstall --user 0 com.xiaomi.mipicks
+adb shell pm uninstall --user 0 com.xiaomi.payment
+adb shell pm uninstall --user 0 com.yandex.zen
+adb shell pm uninstall --user 0 com.zhiliaoapp.musically
+adb shell pm uninstall --user 0 com.yandex.browser
+adb shell pm uninstall --user 0 com.yandex.zenkitpartnerconfig
+adb shell pm uninstall --user 0 com.yandex.preinstallsatellite
+adb shell pm uninstall --user 0 ru.ivi.client
+adb shell pm uninstall --user 0 ru.yandex.searchplugin
+adb shell pm uninstall --user 0 com.linkedin.android
+adb shell pm uninstall --user 0 com.amazon.mShop.android.shopping
+adb shell pm uninstall --user 0 com.block.puzzle.game.hippo.mi
+adb shell pm uninstall --user 0 com.mintgames.triplecrush.tile.fun
+adb shell pm uninstall --user 0 com.sukhavati.gotoplaying.bubble.BubbleShooter.mint
+adb shell pm uninstall --user 0 com.jewelsblast.ivygames.Adventure.free
+adb shell pm uninstall --user 0 com.logame.eliminateintruder3d
+adb shell pm uninstall --user 0 com.kingsgroup.ss.xiaomi
+
+# Huawei
+adb shell pm uninstall --user 0 com.huawei.android.tips # Huawei Tips/Tricks
+adb shell pm uninstall --user 0 com.huawei.android.totemweather # Huawei Weather
+adb shell pm uninstall --user 0 com.huawei.arengine.service # Augmented reality service
+adb shell pm uninstall --user 0 com.huawei.browser # Huawei Browser
+adb shell pm uninstall --user 0 com.huawei.contactscamcard # Business card reader
+adb shell pm uninstall --user 0 com.huawei.compass # Huawei Compass
+adb shell pm uninstall --user 0 com.huawei.desktop.explorer # Desktop Service
+adb shell pm uninstall --user 0 com.huawei.fido.uafclient # Fast ID Online function
+adb shell pm uninstall --user 0 com.huawei.gameassistant # Huawei Game Suite (HiGame)
+adb shell pm uninstall --user 0 com.huawei.geofence # Geofence service
+adb shell pm uninstall --user 0 com.baidu.input_huawei # Built in keyboard
+adb shell pm uninstall --user 0 com.huawei.appmarket # Huawei Market
+adb shell pm uninstall --user 0 com.huawei.android.chr # HwChrService
+adb shell pm uninstall --user 0 com.huawei.android.FloatTasks # Floating dock
+adb shell pm uninstall --user 0 com.huawei.android.hsf # Huawei own Services Framework
+adb shell pm uninstall --user 0 com.huawei.android.hwpay # Huawei Payement app
+adb shell pm uninstall --user 0 com.huawei.android.karaoke # karaoke entertainment app
+adb shell pm uninstall --user 0 com.huawei.android.mirrorshare # MirrorShare from Huawei
+adb shell pm uninstall --user 0 com.huawei.android.remotecontroller # Huawei Smart Controller
+adb shell pm uninstall --user 0 com.huawei.search # HiSearch app
+adb shell pm uninstall --user 0 com.huawei.stylus.floatmenu # Floating menu
+adb shell pm uninstall --user 0 com.huawei.hdiw # Huawei ID app
+adb shell pm uninstall --user 0 com.huawei.hifolder # Huawei Online Cloud folder service
+adb shell pm uninstall --user 0 com.huawei.himovie.overseas # Huawei Videos App
+adb shell pm uninstall --user 0 com.huawei.hitouch # Floating dock by Huawei
+adb shell pm uninstall --user 0 com.huawei.hwdetectrepair # Huawei Smart diagnosis app
+adb shell pm uninstall --user 0 com.huawei.iaware # Huawei info app
+adb shell pm uninstall --user 0 com.huawei.livewallpaper.paradise # Live wallpaper service
+adb shell pm uninstall --user 0 com.huawei.parentcontrol # Parental controls functions
+adb shell pm uninstall --user 0 com.huawei.pcassistant # HiSuite service
+adb shell pm uninstall --user 0 com.huawei.phoneservice # HiCare app
+adb shell pm uninstall --user 0 com.huawei.mirror  # Huawei Mirror app
+adb shell pm uninstall --user 0 com.huawei.screenrecorder # Huawei Screen recorder feature
+adb shell pm uninstall --user 0 com.huawei.vassistant # Huawei Voice Assistant
+adb shell pm uninstall --user 0 com.huawei.videoeditor # Video editor function
+adb shell pm uninstall --user 0 com.huawei.wallet # Huawei Wallet
+adb shell pm uninstall --user 0 com.huawei.watch.sync # Huawei Watch sync function
+adb shell pm uninstall --user 0 com.iflytek.speechsuite # Default voice input method
+
+#Google Bloatware
+adb shell pm uninstall --user 0 com.google.android.projection.gearhead
+adb shell pm uninstall --user 0 com.google.android.apps.docs # Google drive
+adb shell pm uninstall --user 0 com.google.android.gm # GMail
+adb shell pm uninstall --user 0 com.android.vending # Store
+adb shell pm uninstall --user 0 com.google.android.apps.maps # Google Maps
+adb shell pm uninstall --user 0 com.google.android.apps.youtube.music # Music
+adb shell pm uninstall --user 0 com.google.android.apps.subscriptions.red # Google one
+adb shell pm uninstall --user 0 com.google.android.apps.nbu.paisa.user# Pay
+adb shell pm uninstall --user 0 com.google.android.apps.photos #Google Photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon #Google Duo
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox #Google App
+adb shell pm uninstall --user 0 com.google.android.apps.messaging # Message
+adb shell pm uninstall --user 0 com.google.android.apps.books # Google Books
+adb shell pm uninstall --user 0 com.google.android.apps.cloudprint # Cloud print
+adb shell pm uninstall --user 0 com.google.android.apps.currents # Currents
+adb shell pm uninstall --user 0 com.google.android.apps.docs # Google Drive
+adb shell pm uninstall --user 0 com.google.android.apps.fitness # Google Fit
+adb shell pm uninstall --user 0 com.google.android.apps.mapps # Google Maps
+adb shell pm uninstall --user 0 com.google.android.apps.photos # Google Photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon # Google Duo
+adb shell pm uninstall --user 0 com.google.android.apps.wallpaper # Google Wallpapers
+adb shell pm uninstall --user 0 com.google.android.apps.wellbeing # Digital Wellbeing
+adb shell pm uninstall --user 0 com.google.android.feedback # Google Feedback
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox # Google Search
+adb shell pm uninstall --user 0 com.google.android.gm # Gmail
+adb shell pm uninstall --user 0 com.google.ar.core # Google ARCore
+adb shell pm uninstall --user 0 com.google.marvin.talkback # Talkback
+adb shell pm uninstall --user 0 com.google.android.play.games # Google Play Games
+adb shell pm uninstall --user 0 com.google.android.street # Google Street View
+adb shell pm uninstall --user 0 com.google.android.videos # Google Play Movies
+adb shell pm uninstall --user 0 com.google.android.youtube # YouTube
+adb shell pm uninstall --user 0 com.google.tango.measure # Google Measure
+adb shell pm uninstall --user 0 com.android.vending
+adb shell pm uninstall --user 0 com.google.android.music # Google Music
+adb shell pm uninstall --user 0 com.google.android.videos # Play Movies
+adb shell pm uninstall --user 0 com.google.android.apps.magazines #Google News
+adb shell pm uninstall --user 0 com.google.android.apps.docs.editors.sheets #Sheets
+adb shell pm uninstall --user 0 com.google.android.apps.docs.editors.slides #Slides
+adb shell pm uninstall --user 0 com.google.android.apps.docs.editors.docs #Docs
+adb shell pm uninstall --user 0 com.google.android.apps.turbor
+adb shell pm uninstall --user 0 com.google.android.gms.location.history
+adb shell pm uninstall --user 0 com.google.android.apps.googleassistant
+adb shell pm uninstall --user 0 com.google.android.calculator
+adb shell pm uninstall --user 0 com.google.android.calendar
+adb shell pm uninstall --user 0 com.android.chrome
+adb shell pm uninstall --user 0 com.google.android.feedback
+adb shell pm uninstall --user 0 com.google.android.setupwizard
+adb shell pm uninstall --user 0 com.google.android.inputmethod.latin
+adb shell pm uninstall --user 0 com.google.android.keep
+adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.google.ar.core
+adb shell pm uninstall --user 0 com.google.ar.lens
+adb shell pm uninstall --user 0 com.google.android.onetimeinitializer
+
+#Stupider Stuff
+adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.okgoogle
+adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.xgoogle
+adb shell pm uninstall -k --user 0 com.google.android.feedback
+adb shell pm uninstall -k --user 0 com.google.mainline.telemetry
+adb shell pm uninstall -k --user 0 com.google.android.partnersetup
+adb shell pm uninstall -k --user 0 com.amazon.aa # Amazon
+adb shell pm uninstall -k --user 0 com.amazon.aa.attribution # Amazon Attribution
+adb shell pm uninstall -k --user 0 com.booking # Booking App
+adb shell pm uninstall -k --user 0 com.ebay.mobile # Ebay SHopping
+adb shell pm uninstall -k --user 0 com.facebook.appmanager #Facebook manager
+adb shell pm uninstall -k --user 0 com.facebook.system #
+adb shell pm uninstall -k --user 0 com.facebook.orca #Facebook
+adb shell pm uninstall -k --user 0 com.facebook.katana #
+adb shell pm uninstall -k --user 0 com.facebook.services #
+adb shell pm uninstall -k --user 0 com.instagram.android #Instagram
+adb shell pm uninstall -k --user 0 com.netflix.mediaclient # Netflix App
+adb shell pm uninstall -k --user 0 com.netflix.partner.activation #
+adb shell pm uninstall -k --user 0 com.microsoft.translator # Tarnslator from Microsoft
+adb shell pm uninstall -k --user 0 com.snapchat.android # Snapchat
+adb shell pm uninstall -k --user 0 com.twitter.android #Twitter
+adb shell pm uninstall -k --user 0 org.smsalliance.openmobileapi.service # SmartcardService
+#adb shell pm uninstall -k --user 0 com.google.android.gms # Play services
+#adb shell pm uninstall -k --user 0 com.google.android.ims
+#adb shell pm uninstall -k --user 0 com.google.android.tts
+##  adb shell pm install-existing --user 0 com.google.android.gms 
